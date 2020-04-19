f3ec909abddfee97f53c34c45236c8cc0c67eb3e
Chris Deboli <christopher2002buffon@gmail.com>
2020-04-19 09:07:47 +0200

    Creo un metodo "CaricaDatiXML" per caricare i dati di una regione dal file xml

--- RiepilogoDatiCovid19_ChristopherDeboli/MainWindow.xaml.cs
+++ RiepilogoDatiCovid19_ChristopherDeboli/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace RiepilogoDatiCovid19_ChristopherDeboli
 {
@@ -37,6 +38,61 @@ namespace RiepilogoDatiCovid19_ChristopherDeboli
             else
             {
                 MessageBox.Show("Devi inserire una regione", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Warning); // Avverto all'utente che deve inserire una regione
+            }
+        }
+
+        // Metodo che carica i dati dal file xml e stampa le informazione di una regione richiesta
+        private void CaricaDatiXML()
+        {
+            string path = @"RegioniCovid19.xml";
+            XDocument xmlLoad = XDocument.Load(path);
+            XElement xmlRoot = xmlLoad.Element("root");
+            var xmlRow = xmlRoot.Elements("row");
+
+            foreach (var item in xmlRow)
+            {
+                XElement xmlData = item.Element("data");
+                XElement xmlStato = item.Element("stato");
+                XElement xmlCodiceRegione = item.Element("codice_regione");
+                XElement xmlDenominazioneRegione = item.Element("denominazione_regione");
+                XElement xmlLat = item.Element("lat");
+                XElement xmlLong = item.Element("long");
+                XElement xmlRicoverati = item.Element("ricoverati_con_sintomi");
+                XElement xmlTerapiaIntensiva = item.Element("terapia_intensiva");
+                XElement xmlOspedalizzati = item.Element("totale_ospedalizzati");
+                XElement xmlIsolamentoDomiciliare = item.Element("isolamento_domiciliare");
+                XElement xmlPositivi = item.Element("totale_positivi");
+                XElement xmlVariazionePositivi = item.Element("variazione_totale_positivi");
+                XElement xmlNuoviPositivi = item.Element("nuovi_positivi");
+                XElement xmlDimessiGuariti = item.Element("dimessi_guariti");
+                XElement xmlDeceduti = item.Element("deceduti");
+                XElement xmlTotaleCasi = item.Element("totale_casi");
+                XElement xmlTamponi = item.Element("tamponi");
+
+                Regione regione = new Regione();
+                {
+                    regione.Data = Convert.ToDateTime(xmlData.Value);
+                    regione.Stato = xmlStato.Value;
+                    regione.CodiceRegione = Convert.ToInt32(xmlCodiceRegione.Value);
+                    regione.Denominazione_Regione = xmlDenominazioneRegione.Value;
+                    regione.Latitudine = Convert.ToSingle(xmlLat.Value);
+                    regione.Longitudine = Convert.ToSingle(xmlLong.Value);
+                    regione.Ricoverati = Convert.ToInt32(xmlRicoverati.Value);
+                    regione.Terapia_Intensiva = Convert.ToInt32(xmlTerapiaIntensiva.Value);
+                    regione.Ospedalizzati = Convert.ToInt32(xmlOspedalizzati.Value);
+                    regione.Isolamento_Domicìlianare = Convert.ToInt32(xmlIsolamentoDomiciliare.Value);
+                    regione.Positivi = Convert.ToInt32(xmlPositivi.Value);
+                    regione.Variazione_Positivi = Convert.ToInt32(xmlVariazionePositivi.Value);
+                    regione.Nuovi_Positivi = Convert.ToInt32(xmlNuoviPositivi.Value);
+                    regione.Dimessi_Guariti = Convert.ToInt32(xmlDimessiGuariti.Value);
+                    regione.Deceduti = Convert.ToInt32(xmlDeceduti.Value);
+                    regione.Totale_Casi = Convert.ToInt32(xmlTotaleCasi.Value);
+                    regione.Tamponi = Convert.ToInt32(xmlTamponi.Value);
+                }
+
+                // Controllo se esiste la regione ricercata
+                if (Txt_Regione.Text.Contains(regione.Denominazione_Regione))
+                    Lst_RiepilogoDati.Items.Add(regione); // Stampo i dati della regione ricercata
             }
         }
     }
